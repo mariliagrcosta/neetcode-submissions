129dad398e1a6ae1f586fcfa208384f534ff7e15
Marília Costa <51496973+mariliagrcosta@users.noreply.github.com>
2026-05-17 19:48:19 -0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+        t_dict = {}
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+        return s_dict == t_dict
