5dcd806d1aa08dd65bf11ddae853d044df0b293d
Marília Costa <51496973+mariliagrcosta@users.noreply.github.com>
2026-05-19 01:32:16 -0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        checked = {}
+        for i in range (len(nums)):
+            complement = target - nums[i]
+            if complement in checked:
+                return [checked[complement], i]
+            checked[nums[i]] = i
