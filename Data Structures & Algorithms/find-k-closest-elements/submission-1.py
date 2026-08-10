31c25cb32267b30bb2f84956b04639ea3205de37
Marília Costa <51496973+mariliagrcosta@users.noreply.github.com>
2026-08-10 00:47:51 -0300

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        right = bisect_left(arr, x)
+        left = right - 1
+
+        for _ in range(k):
+            if left < 0:
+                right += 1
+            elif right >= len(arr):
+                left -= 1
+            elif x - arr[left] <= arr[right] - x:
+                left -= 1
+            else:
+                right += 1
+
+        return arr[left + 1:right]
