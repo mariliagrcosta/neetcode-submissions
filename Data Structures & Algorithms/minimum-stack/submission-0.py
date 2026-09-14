4993c1c1522d369b4400747ee4f58764ff6a4e99
Marília Costa <51496973+mariliagrcosta@users.noreply.github.com>
2026-09-14 10:08:44 -0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,22 @@
+class MinStack:
+    def __init__(self):
+        self.main_stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.main_stack.append(val)
+
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        top_value = self.main_stack.pop()
+
+        if top_value == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.main_stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
